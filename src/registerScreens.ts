1df80437fb893701117737a66a903eeda32f9756
Karl Marx Lopez <karlmarx.webdev@gmail.com>
2020-09-12 00:32:57 +0800

    feat: remove prefix support

--- src/registerScreens.ts
+++ src/registerScreens.ts
@@ -1,32 +1,28 @@
-import { Navigation } from "react-native-navigation";
-import { ComponentProvider } from "react-native";
-import { ComponentType } from "react";
-import { Screen, IScreenName } from "..";
+import {Navigation} from 'react-native-navigation';
+import {ComponentProvider} from 'react-native';
+import {ComponentType} from 'react';
+import {ScreenFC} from './types';
 
 /**
  * Util function that wraps Navigation.registerComponent
  * @param screens Array of screen component
- * @param prefix Screen name prefix, usually this is the App name
  */
-function registerScreens(
-  screens: Screen[],
-  prefix?: string,
-  customComponentProvider?: (component: ComponentType<any>) => ComponentType<any>
+export function registerScreens(
+  screens: ScreenFC[],
+  componentProvider?: (component: ComponentType<any>) => ComponentType<any>,
 ): ComponentProvider[] {
-  return screens.map(screen => {
-    const s = screen as IScreenName;
-    const screenName = (s.screenName = `${prefix ? `${prefix}.` : ''}${
-      s.screenName
-    }`);
+  return screens.map((screen) => {
+    const {screenName} = screen;
+
     return Navigation.registerComponent(
       screenName,
       () =>
-        // @ts-ignore FIXME! Ignore ComponentType<any> errors for now
-        customComponentProvider ? customComponentProvider(s) : screen,
-      // @ts-ignore Type '() => IScreenName' is not assignable to type 'ComponentProvider'.
-      customComponentProvider ? () => s : undefined
+        typeof componentProvider === 'function'
+          ? componentProvider(screen as ComponentType<any>)
+          : (screen as ComponentType<any>),
+      typeof componentProvider === 'function'
+        ? () => screen as ComponentType<any>
+        : undefined,
     );
   });
 }
-
-export default registerScreens;
